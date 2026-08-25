d940753ee5a056da0bc934ab9870bb503dfdfa7a
gustavmagno <gustavosocial01.gs@pm.me>
2026-08-24 22:39:24 -0300

    teste-finalizado

--- Ex000/script.js
+++ Ex000/script.js
@@ -1,11 +1,11 @@
-var VelAtual = document.querySelector('input#m1');
 var BtnVer = document.querySelector('input#v1');
-v1.addEventListener = ('click', clicar());
+BtnVer.addEventListener('click', clicar);
+const VelPerm = 50;
 
 function clicar() {
-    var VelPerm = 50;
-    if(m1 > 50) {
-        var ValSentenca = document.querySelector('div#resultado');
+    var VelAtual = Number(document.querySelector('input#m1').value);
+    var ValSentenca = document.querySelector('div#resultado');
+    if(VelAtual > VelPerm) {
         ValSentenca.innerHTML = `Você ultrapassou a velocidade permitida de ${VelPerm} Km/h. MULTADO`
     } else {
         ValSentenca.innerHTML = 'Parabéns! Você fez o mínimo!'
